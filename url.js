99e10fac0e58b065b0fd88e9fe20843da2d7ca33
Rwen037 <150349478+Rwen037@users.noreply.github.com>
2025-06-14 15:24:08 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,10 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdFImtmyVHi3mkd26KGn7gTuSAIahoDuq_w_uP39GcNc4-AJQ/viewform?usp=header'
+	'https://docs.google.com/forms/d/e/1FAIpQLSe-sb-xh-4Tbi37k07qCqj1J-osQsik-B5b2C13qINAJI6M5A/viewform?usp=header'
+	'https://docs.google.com/forms/d/e/1FAIpQLSf-LRzWLHupRBkG5GWI-6TVIFUBvRzvhE0rcXt0XIWPYvIiSQ/viewform?usp=header'
+	'https://docs.google.com/forms/d/e/1FAIpQLSfwQEYT8mimY5HZ-SF-OTCAaYgLqlBzNVXMswuMq1D9Sf0qVQ/viewform?usp=header'
+	'https://docs.google.com/forms/d/e/1FAIpQLSeYn1v7Z_gabPfuDXNmFuCLtU2FVv78uoQUDi75QnX9fz80Og/viewform?usp=header'
+	'https://docs.google.com/forms/d/e/1FAIpQLSfae3Bsuh1X8bc-b4kM1qh2Ru4uzaAMOdf50d1CMN7ub4lUcg/viewform?usp=header'
 ];
